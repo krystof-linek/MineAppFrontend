71d7d73fd271a5ac72da9a7f21169aff4b3a9fd4
Kryštof Linek <97353954+krystof-linek@users.noreply.github.com>
2022-08-25 13:01:08 +0200

    Update DesktopLayout.vue

--- src/components/DesktopLayout.vue
+++ src/components/DesktopLayout.vue
@@ -66,11 +66,12 @@
     <!-- Mobile navigation -->
     <nav v-if="$vuetify.breakpoint.smAndDown && isMenuShowed">
         <v-list class="orange pb-0"> 
-          <v-list-item v-if="isUserLogged">
-            <v-list-item-content class="text-center">
-              <v-list-item-avatar rounded="0">
-                <v-img src="https://minotar.net/avatar/krystoflinek"></v-img>
-              <v-list-item-avatar>
+          <v-list-item v-if="isUserLogged" justify="center">
+            <v-list-item-avatar rounded="0">
+              <v-img src="https://minotar.net/avatar/krystoflinek"></v-img>
+            <v-list-item-avatar>
+            
+            <v-list-item-content>
               <v-list-item-title>
                 krystoflinek
               </v-list-item-title>
