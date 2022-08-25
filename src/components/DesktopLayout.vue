240e037b30852c55eaa41d866909b85d8901e193
Kryštof Linek <97353954+krystof-linek@users.noreply.github.com>
2022-08-25 11:19:23 +0200

    Update DesktopLayout.vue

--- src/components/DesktopLayout.vue
+++ src/components/DesktopLayout.vue
@@ -92,7 +92,7 @@
       <v-list v-if="isUserLogged">
         <v-list-item v-for="n in 5" :key="n" link>
             <v-list-item-content>
-              <v-list-item-title>
+              <v-list-item-title class="text-center">
                 Item {{ n }}
               </v-list-item-title>
             </v-list-item-content>
